e1bd2f8ba07020408ebb9ecb3fc060cc19c4149d
aswinachuvappat <aswin2001vappat@gmail.com>
2023-12-13 14:25:59 +0530

    commit and

---
+++ bin/uneryopperationexample.dart
@@ -0,0 +1,17 @@
+void main(){
+  int x=1;
+  // print(x++); //first print x then add 1
+  // print(x);   //
+  // print(++x);//first add 1 then print x
+  print(x++);
+  print(x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(x--);
+  print(x--);
+  print(x--);
+  print(x--);
+  print(x--);
+
+}
